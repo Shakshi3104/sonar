dffbf7eae1fbdec9999763903c379aed19c5ba79
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2022-10-23 15:42:34 +0900

    Initial Commit

---
+++ sonar/sonarApp.swift
@@ -0,0 +1,17 @@
+//
+//  sonarApp.swift
+//  sonar
+//
+//  Created by MacBook Pro M1 on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct sonarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
